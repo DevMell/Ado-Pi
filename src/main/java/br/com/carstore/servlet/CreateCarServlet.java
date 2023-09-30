30496c704da8bba97de44bac82ec9d2ee3bcd516
Melissa Mendes <129010566+DevMell@users.noreply.github.com>
2023-09-29 21:26:10 -0300

    Add files via upload

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,13 +16,20 @@ public class CreateCarServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        super.doPost(request, response);
-
         String carName = request.getParameter("car-name");
 
         System.out.println(carName);
 
         request.getRequestDispatcher("index.html").forward(request, response);
+
+        CarDao dao = new CarDao();
+        Car carro = new Car();
+
+        carro.setName(carName);
+        dao.createCar(carro);
+
+
+
     }
 
 }
